378a737300858a3fa29658a264a762a6e4cf497e
Sooraj <soorajantony@live.in>
2017-05-11 17:35:21 +0530

    Initial update from https://github.com/suzukikijai/mail-tracker

---
+++ README.md~
@@ -0,0 +1,1 @@
+Prototype mail based Web Beacon custom script
---
+++ database/migrations/2016_11_10_213551_add-message-id-to-sent-emails-table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use jdavidbakr\MailTracker\Model\SentEmail;
+
+class AddMessageIdToSentEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection((new SentEmail())->getConnectionName())->table('sent_emails', function(Blueprint $table) {
+            $table->string('message_id')->nullable();
+            $table->text('meta');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection((new SentEmail())->getConnectionName())->table('sent_emails', function(Blueprint $table) {
+            $table->dropColumn('message_id');
+            $table->dropColumn('meta');
+        });
+    }
+}
